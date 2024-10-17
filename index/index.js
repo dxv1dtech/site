6bdfe0c7045016bb3d819fdb68cb8f09c1c627fb
dxv1dtech <petrache_david_andrei@yahoo.com>
2024-10-17 21:21:03 +0300

    deleted the folder

--- index/index.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("toggleBtn");
-const navLinks = document.getElementById("navLinks");
-
-toggleBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("active"); // Toggle the active class
-});
